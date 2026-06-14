7ed2c7cc8af40932d54bd622d374ffa1deb55a50
Brendan Forster <brendandforster@gmail.com>
2026-06-14 14:50:16 -0700

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -60,7 +61,24 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+  {children}
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-G2HZEZPEXL"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-G2HZEZPEXL');
+    `}
+  </Script>
+</body>
     </html>
   );
 }
